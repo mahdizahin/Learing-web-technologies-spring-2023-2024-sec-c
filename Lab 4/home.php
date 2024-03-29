792154728f8c007873fd5f1121722f0170ae5438
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2024-03-30 01:17:14 +0600

    Create home.php

---
+++ Lab 4/home.php
@@ -0,0 +1,13 @@
+<?php
+    if(!isset($_COOKIE['flag'])){
+        header('location: login.html');
+    }
+?>
+<html lang="en">
+<head>
+    <title>home</title>
+</head>
+<body>
+    <h1>Welcome home</h1>
+</body>
+</html>
