0fb3a3238ddd421543b89c6296067fcef97103a0
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2024-03-30 01:08:49 +0600

    Update task2.php

--- Lab 4/task2.php
+++ Lab 4/task2.php
@@ -1,11 +1,21 @@
-<?php
+ <html lang="en">
 
-function vatcalculation($amount){
- $vatRate = 0.15;
- return $amount*$vatRate;
-}
+<head>
+    <title>ChangePassword </title>
+</head>
 
-$amount=1000;
-echo "Vat of {$amount} is : ". vatcalculation($amount);
+<body>
+    <form method="post" action="passwordChangeCheck.php" enctype="">
+        <fieldset>
+            <legend>PASSWORD CHANGE</legend>
+            Current Password: <input type="password" name="currpassword" value="" /> <br>
+            New Password: <input type="password" name="newpassword" value="" /> <br>
+            Retype New Password: <input type="password" name="retypepassword" value="" /> <br>
+            <hr>
+            <input type="submit" name="submit" value="Submit" />
+        </fieldset>
 
-?>
+    </form>
+</body>
+
+</html>
